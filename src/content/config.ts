5bcc4b1ab6e52c7303a6a19683c822fada96c333
Fran Rey <franreycastedo@gmail.com>
2025-05-15 21:56:13 +0200

    add: content loads in order

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,6 +1,8 @@
 import { defineCollection, z } from "astro:content";
+import { glob, file } from 'astro/loaders';
 
 const works = defineCollection({
+    loader: glob({ pattern: "**/*.md", base: "./src/content/works" }),
     schema: z.object({
         title: z.string(),
         description: z.string(),
